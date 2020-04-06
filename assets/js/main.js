3c98a0586d568fba24f751f7f728f53a37f0659b
sankshitpandoh <sankshit02pandoh.sp@gmail.com>
2020-04-06 20:07:31 +0530

    browser storage accessed and little functionality added

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,11 +2,13 @@ var userName = window.localStorage.getItem("userNAME")
 // window.localStorage.removeItem("userNAME")
 
 document.getElementById("user-name").innerHTML = userName
+document.getElementById("greeting-user").innerHTML = "Welcome, " + "<span>" +userName+ "</span>"
+
 var tagLine
 tagLine = "Hey there, I'm using Groundpe"
 document.getElementById("status").innerHTML = tagLine
 document.getElementById("tag-line").value = tagLine
-document.getElementById("greeting-user").innerHTML = "Welcome, " + "<span>" +userName+ "</span>"
+
 function statusUpdate(){
     tagLine = document.getElementById("tag-line").value
     document.getElementById("status").innerHTML = tagLine
