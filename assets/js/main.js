ea1373245a6cc160adc31cc403bf150edffea59b
sankshitpandoh <sankshit02pandoh.sp@gmail.com>
2020-04-07 12:37:35 +0530

    all navigation routes covered

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,7 @@
 var userName = window.localStorage.getItem("userNAME")
-// window.localStorage.removeItem("userNAME")
 
 document.getElementById("user-name").innerHTML = userName
-document.getElementById("greeting-user").innerHTML = "Welcome, " + "<span>" +userName+ "</span>"
+
 
 var chat_data = []
 
@@ -18,16 +17,47 @@ get_data.onreadystatechange = function(){
 get_data.open("GET","people.json" , true)
 get_data.send()
 
+var welcomePage
+var load_data = new XMLHttpRequest()
+load_data.onreadystatechange = function(){
+    if(this.readyState ==4 && this.status == 200){
+        welcomePage = this.response
+        document.getElementById("right-bar").innerHTML = this.response
+        document.getElementById("greeting-user").innerHTML = "Welcome, " + "<span>" +userName+ "</span>" 
+        document.getElementById("tag-line").value = tagLine 
+    }
+}
+load_data.open("GET", "welcome.html" , true)
+load_data.send()
+
+function editProfile(){
+    document.getElementById("right-bar").innerHTML = welcomePage
+    document.getElementById("greeting-user").innerHTML = "Welcome, " + "<span>" +userName+ "</span>" 
+    document.getElementById("tag-line").value = tagLine 
+}
+
 var tagLine
 tagLine = "Hey there, I'm using Groundpe"
 document.getElementById("status").innerHTML = tagLine
-document.getElementById("tag-line").value = tagLine
 
 
 function statusUpdate(){
     tagLine = document.getElementById("tag-line").value
     document.getElementById("status").innerHTML = tagLine
 }
+
+menuOpen = false
+function dpDown(){
+    if(menuOpen == false){
+        document.getElementById("menu-dp-down").style.display = "block"
+        menuOpen = true
+    }
+    else if(menuOpen == true){
+        document.getElementById("menu-dp-down").style.display = "none"
+        menuOpen = false
+    }    
+}
+
 
 function startConv(){
     document.getElementById("pop-space").innerHTML = "<div id='pop-up'><div id='inner-pop'><div id='inner-top-pop'><i id='close' class='fa fa-times' onclick='closeStartConv()'></i></div><div id='main-top-pop'></div></div></div>" 
@@ -41,7 +71,6 @@ function closeStartConv(){
 }
 
 function openChat(chat_id){
-    // console.log(chat_data.people[chat_id])
     var pull_data = new XMLHttpRequest()
     pull_data.onreadystatechange = function(){
         if(this.readyState == 4 && this.status == 200){
@@ -51,4 +80,9 @@ function openChat(chat_id){
     }
     pull_data.open("GET", "chatPage.html" , true)
     pull_data.send()
+}
+
+function logOut(){
+    window.localStorage.removeItem("userNAME")
+    window.location.replace("../index.html")
 }
